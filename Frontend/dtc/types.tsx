8e72218f155c2bf4af5280ad657da401fd4448b5
taehonaldo <tae_ho_@naver.com>
2022-02-21 20:49:54 +0900

    Front / fix type for navigation

--- Frontend/dtc/types.tsx
+++ Frontend/dtc/types.tsx
@@ -15,6 +15,7 @@ declare global {
 
 export type RootStackParamList = {
   Root: NavigatorScreenParams<RootTabParamList> | undefined;
+  Landing: undefined;
   Modal: undefined;
   NotFound: undefined;
 };
@@ -25,8 +26,9 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 >;
 
 export type RootTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Recom: undefined;
+  Chat: undefined;
+  Profile: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
