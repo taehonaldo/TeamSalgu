95e892bbe59fd474d59aebf0e6f457e5e23d34f6
taehonaldo <tae_ho_@naver.com>
2022-04-07 19:00:06 +0900

    init android (Kotlin)

--- Frontend/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Frontend/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //test
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
